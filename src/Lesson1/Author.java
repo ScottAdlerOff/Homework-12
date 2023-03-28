97c59a389a6f8add4e1e125413b03b336b94b5a2
Scott Adler <enclave38@gmail.com>
2023-03-28 19:58:21 +0300

    Домашняя работа №12

---
+++ src/Lesson1/Author.java
@@ -0,0 +1,21 @@
+package Lesson1;
+
+public class Author {
+    String firstName;
+    String secondName;
+
+    public Author(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getSecondNameName() {
+        return this.secondName;
+    }
+
+}
+
+
